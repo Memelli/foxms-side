95fba1dee4662144a71fdf6d2876c1ad7cc28f92
Julio Memeli <memeli>
2021-05-23 17:21:46 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink } from './FooterElements'
 
 const Footer = () => {
     return (
+        <>
         <FooterContainer>
             <FooterWrap>
                 <FooterLinksContainer>
@@ -15,10 +17,38 @@ const Footer = () => {
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                         </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                        </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
             </FooterWrap>
         </FooterContainer>
+        </>
     )
 }
 
