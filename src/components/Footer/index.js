e6463e2c94a10e0bb12dc56a2bc5b6ee2497c6eb
Julio Memeli <memeli>
2021-05-24 21:36:05 -0300

    footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink } from './FooterElements'
+import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebSiteRights, SocialIcons, SocialIconLink } from './FooterElements'
+
+import { FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
     return (
         <>
         <FooterContainer>
             <FooterWrap>
-                <FooterLinksContainer>
+                {/* <FooterLinksContainer>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
                             <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
@@ -15,7 +17,7 @@ const Footer = () => {
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
-                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                
                         </FooterLinkItems>
                         <FooterLinkItems>
                             <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
@@ -24,7 +26,7 @@ const Footer = () => {
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
-                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                
                         </FooterLinkItems>
                         <FooterLinkItems>
                             <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
@@ -33,7 +35,7 @@ const Footer = () => {
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
-                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                
                         </FooterLinkItems>
                         <FooterLinkItems>
                             <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
@@ -42,10 +44,38 @@ const Footer = () => {
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Lorem Ipsum</FooterLinkTitle>
                                 <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                <FooterLink to="/">Lorem Ipsum act</FooterLink>
+                                
                         </FooterLinkItems>
                     </FooterLinksWrapper>
-                </FooterLinksContainer>
+                </FooterLinksContainer> */}
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            memelli.
+                        </SocialLogo>
+                        <WebSiteRights>memelli © {new Date().getFullYear()} todos os direitos reservados.</WebSiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+                                <FaLinkedin />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.github.com/memelli" target="_blank" aria-label="Github">
+                                <FaGithub />
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                    </SocialMedia>
             </FooterWrap>
         </FooterContainer>
         </>
